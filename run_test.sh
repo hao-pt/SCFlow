ab5d8ee2e3ab48da3e95d247ded6433ce76def9d
quandm7 <v.quandm7@vinai.io>
2023-11-02 15:39:28 +0700

    update

--- run_test.sh
+++ run_test.sh
@@ -37,21 +37,21 @@ export PYTHONPATH=$(pwd):$PYTHONPATH
 
 MODEL_TYPE=adm
 EPOCH_ID=425
-DATASET=lsun_bedroom
-EXP=laflo_bed_f8_lr5e-5
+DATASET=ffhq_256
+EXP=ffhq_f8_lr2e-5_adm
 METHOD=dopri5
 STEPS=0
-USE_ORIGIN_ADM=False
+USE_ORIGIN_ADM=True
 
-if [[ ${USE_ORIGIN_ADM} == train ]]; then
+if [[ ${USE_ORIGIN_ADM} == True ]]; then
     python test_flow_latent.py --exp ${EXP} \
         --dataset ${DATASET} --batch_size 100 --epoch_id ${EPOCH_ID} \
         --image_size 256 --f 8 --num_in_channels 4 --num_out_channels 4 \
-        --nf 256 --ch_mult 1 2 3 4 --attn_resolution 16 8 --num_res_blocks 2 \
+        --nf 256 --ch_mult 1 2 3 4 --attn_resolution 16 8 4 --num_res_blocks 2 \
         --use_origin_adm \
         --master_port $MASTER_PORT --num_process_per_node 1 \
-        --compute_fid --output_log ${EXP}_${EPOCH_ID}_${METHOD}${STEPS}.log \
         --method dopri5 --num_steps 0 \
+        # --compute_fid --output_log ${EXP}_${EPOCH_ID}_${METHOD}${STEPS}.log \
         # --measure_time \
         # --use_karras_samplers \
         # --method heun --step_size 50 \
